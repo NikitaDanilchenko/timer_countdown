323ff9d6a21dbde6c409fe43b8818c50c0b4df2d
Nikita Danilchenko <nick92dan98@gmail.com>
2023-11-13 21:13:34 +0300

    new commit

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Timer from './components/Timer';
+import Countdown from './components/Countdown';
+// import logo from './assets/icons/logo.svg';
+// import {SApp, SHeader, SLink, SLogo} from "./assets/styles/app.styles";
+
+function App() {
+    return (
+        <div>
+            <Timer />
+            <Countdown />
+        </div>
+    );
+}
+
+export default App;
